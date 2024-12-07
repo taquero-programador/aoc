537f249a371a0cb299a0bcbe7511ce221d77d4aa
taquero-programador <jrangel93@proton.me>
2024-12-06 18:20:06 -0600

    feat(day 04) 04 solved!

--- 2024/03/03.sh
+++ 2024/03/03.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat input.txt | grep -Eio 'mul\([[:digit:]]{1,3}\,[[:digit:]]{1,3}\)' | grep -Eio '[[:digit:]]{1,3}\,[[:digit:]]{1,3}' > output.txt
+grep -Eio 'mul\([[:digit:]]{1,3}\,[[:digit:]]{1,3}\)' input.txt | grep -Eio '[[:digit:]]{1,3}\,[[:digit:]]{1,3}' > output.txt
 
 python 03.py
